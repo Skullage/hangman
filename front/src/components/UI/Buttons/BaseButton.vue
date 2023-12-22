23b8d5c6baf128cfaa80d807317c589de9397bff
evgenydogonadze <dogonadze1999@gmail.com>
2023-12-22 16:16:47 +0500

    Правки дизайна

--- front/src/components/UI/Buttons/BaseButton.vue
+++ front/src/components/UI/Buttons/BaseButton.vue
@@ -11,7 +11,7 @@ const props = defineProps({
   <button
     type="button"
     :disabled="props.disabled"
-    class="disabled:!bg-black disabled:border-transparent disabled:hover:!border-transparent disabled:cursor-default dark:bg-fifthDark bg-fourthLight hover:border-secondaryLight rounded-lg border-2 dark:hover:border-thirdDark px-4 py-2 font-bold duration-300"
+    class="disabled:!bg-black disabled:!border-transparent disabled:hover:!border-transparent disabled:cursor-default dark:bg-fifthDark bg-fourthLight hover:border-secondaryLight rounded-lg border-2 border-transparent dark:hover:border-thirdDark px-4 py-2 font-bold duration-300"
   >
     <slot></slot>
   </button>
